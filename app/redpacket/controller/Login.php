2a8df553477f7d1deb5b20b9a01b435fc93d50ae
mayuan-PC\mayuan <1047457825@qq.com>
2018-10-29 23:09:21 +0800

    登陆未完成

--- app/redpacket/controller/Login.php
+++ app/redpacket/controller/Login.php
@@ -5,7 +5,14 @@ use think\Controller;
 
 class Login extends Controller{
     public function index(){
-        echo "登录页";
+       
+        return $this->fetch('login');
+    }
+    
+    public function doLogin(){
+        #获取数据
+        $data = input('param.');
+        var_dump($data);
     }
 }
 
